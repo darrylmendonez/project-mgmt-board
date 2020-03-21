9a0c1589bbb18858827430eb7cca1905c0939eda
Darryl.Mendonez <darryl.mendonez@Laptop.home>
2020-03-20 22:10:16 -0400

    Updates

---
+++ precache-manifest.49f9191be5f5da53af778e79a30cbc0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "734bb102b17107c4d76cb4a57f04ffb6",
+    "url": "/project-mgmt-board/index.html"
+  },
+  {
+    "revision": "a34bf2f9d7c0881ecb13",
+    "url": "/project-mgmt-board/static/js/2.781279a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-mgmt-board/static/js/2.781279a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0ac39ece75f597a8f91",
+    "url": "/project-mgmt-board/static/js/main.7209f2f6.chunk.js"
+  },
+  {
+    "revision": "741849746f82339ecb30",
+    "url": "/project-mgmt-board/static/js/runtime-main.1b22670c.js"
+  }
+]);
